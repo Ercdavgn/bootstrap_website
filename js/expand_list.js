b4e76338ed45ccda3b0c04e60e716040b231bd19
Eric <ercdavgn@gmail.com>
2016-12-11 13:48:18 -0500

    Create expand_list.js

---
+++ js/expand_list.js
@@ -0,0 +1,266 @@
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.8.3.js"></script>
+        <script src="/disablelinkscroll.js" type="text/javascript"></script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxA').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionA, .AnswerA').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxB').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionB, .AnswerB').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxC').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionC, .AnswerC').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxD').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionD, .AnswerD').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxE').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionE, .AnswerE').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxF').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionF, .AnswerF').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxG').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionG, .AnswerG').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxH').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionH, .AnswerH').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxI').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionI, .AnswerI').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxJ').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionJ, .AnswerJ').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxK').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionK, .AnswerK').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxL').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionL, .AnswerL').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxM').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionM, .AnswerM').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxN').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionN, .AnswerN').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxO').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionO, .AnswerO').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxP').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionP, .AnswerP').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxQ').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionQ, .AnswerQ').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxR').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionR, .AnswerR').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxS').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionS, .AnswerS').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxT').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionT, .AnswerT').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxU').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionU, .AnswerU').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
+        <script type="text/javascript">
+          $(function() {
+            $('.textboxV').click(function(x) {
+              var tempScrollTop = $(window).scrollTop();
+              $('.QuestionV, .AnswerV').toggle();
+              $(window).scrollTop(tempScrollTop);
+              x.preventDefault();
+              return false;
+            });
+          });
+
+        </script>
